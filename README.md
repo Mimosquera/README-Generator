36688970d5146dd865d22430df8042ea977bde7a
Michael Mosquera <michael2000ny@gmail.com>
2024-11-11 10:27:20 -0500

    finished code

---
+++ README.md
@@ -0,0 +1,20 @@
+
+    # README Generator
+
+    ## Description
+    Generates READMEs.
+
+    ## Installation
+    N/A
+
+    ## Usage
+    Run the index.js file with Node and answer the questions.
+
+    assets/README Walkthrough.mp4
+
+    ## Credits
+    Michael Mosquera, Expert Learning Assistant AI
+
+    ## License
+    N/A
+    
--- index.js
+++ index.js
@@ -1,13 +1,61 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Array of questions for user input
+const questions = [
+  { type: 'input', name: 'title', message: 'What is the project title?' },
+  { type: 'input', name: 'description', message: 'Provide a description:' },
+  { type: 'input', name: 'installation', message: 'What are the steps required for installation?' },
+  { type: 'input', name: 'usage', message: 'Usage instructions:' },
+  { type: 'input', name: 'credits', message: 'Credits:' },
+  { type: 'input', name: 'license', message: 'License:' },
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Function to write README file
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      console.error("Error writing file:", err);
+    } else {
+      console.log(`File ${fileName} has been successfully created!`);
+    }
+  });
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+// Function to generate README content
+function generateMarkdown(data) {
+  return `
+    # ${data.title}
+
+    ## Description
+    ${data.description}
+
+    ## Installation
+    ${data.installation}
+
+    ## Usage
+    ${data.usage}
+
+    ## Credits
+    ${data.credits}
+
+    ## License
+    ${data.license}
+    `;
+}
+
+// Function to initialize app
+function init() {
+  inquirer
+    .prompt(questions)
+    .then((answers) => {
+      const readmeContent = generateMarkdown(answers);
+      writeToFile('README.md', readmeContent);
+    })
+    .catch((error) => {
+      console.error("Error initializing app:", error);
+    });
+}
 
 // Function call to initialize app
 init();
